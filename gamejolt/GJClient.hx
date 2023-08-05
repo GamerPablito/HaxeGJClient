28a8150187eaae87444dc140f01103a44b51c3a2
GamerPablito <leonpablo1998@gmail.com>
2023-08-04 22:36:04 -0600

    Final remasterization!

--- gamejolt/GJClient.hx
+++ gamejolt/GJClient.hx
@@ -9,7 +9,6 @@ import haxe.crypto.Sha1;
 import lime.app.Application;
 import lime.app.Future;
 import lime.app.Promise;
-import lime.utils.Bytes;
 import sys.thread.Thread;
 
 using StringTools;
@@ -109,16 +108,14 @@ class GJClient {
 	var game:{id:Int, key:String};
 	var user:{name:String, token:String} = {name: '', token: ''};
 	var pingState:String = 'active';
-	var debugNotes:Bool = false;
 
 	/**
 	 * Makes a new `GJClient` constructor.
 	 * @param game The ID and Private Key of your game goes here.
 	 * @param pingInterval Interval in seconds for the Client to make ping session signals to GameJolt. Default is 3.
 	 */
-	public function new(game:{id:Int, key:String}, pingInterval:Float = 3, debugNotes:Bool = false) {
+	public function new(game:{id:Int, key:String}, pingInterval:Float = 3) {
 		this.game = game;
-		this.debugNotes = debugNotes;
 		Application.current.window.onFocusIn.add(() -> pingState = 'active');
 		Application.current.window.onFocusOut.add(() -> pingState = 'idle');
 
@@ -127,11 +124,8 @@ class GJClient {
 		if (pingInterval > 60)
 			pingInterval = 60;
 
-		new Timer(pingInterval * 1000).run = () -> if (request('sessions', 'ping', ['status' => pingState]).success != 'true'
-			&& session != null) logout();
-
-		if (debugNotes)
-			trace('GJClient Initialized! | Ping Interval: ${pingInterval}s');
+		new Timer(pingInterval * 1000).run = () -> Thread.create(pingSession);
+		trace('GJClient Initialized! | Ping Interval: ${pingInterval}s');
 	}
 
 	/**
@@ -140,147 +134,176 @@ class GJClient {
 	 * @return A `Future` instance where you can set actions if this got success or failure.
 	 */
 	public function login(newUser:Null<{name:String, token:String}>):Future<User> {
-		var process = new Promise<User>();
+		user = newUser;
+		var promise = new Promise<User>();
 
-		if (newUser.name != '' && newUser.token != '') {
-			if (session == null) {
-				user = newUser;
-				var auth = request('users', 'auth');
-				if (auth.success == 'true' && !isSessionActive()) {
-					var connect = request('sessions', 'open');
-					if (connect.success == 'true') {
-						var userInfo = getUserData();
-						var friends = getFriendsList();
-						var trophies = getTrophiesList();
+		Thread.create(function() {
+			var auth = request("users", "auth");
+			promise.progress(1, 3);
+			if (auth.message == null) {
+				var connect = request("sessions", "open");
+				promise.progress(2, 3);
+				if (connect.message == null) {
+					var u = getUserData();
+					var t = getTrophiesList();
+					var f = getFriendsList();
+					promise.progress(3, 3);
 
-						if (userInfo != null && friends != null && trophies != null) {
-							session = {info: userInfo, friends: friends, trophies: trophies};
-							process.complete(userInfo);
-						} else {
-							user = {name: "", token: ""};
-							logout(() -> process.error("Failed to fetch session data!"));
-						}
+					if (u != null && t != null && f != null) {
+						session = {info: u[0], trophies: t, friends: f};
+						promise.complete(u[0]);
 					} else {
-						user = {name: "", token: ""};
-						process.error(connect.message != null ? connect.message : "Something went wrong during logging in!");
+						logout();
+						promise.error("Failed to fetch session data");
+						return;
 					}
 				} else {
-					user = {name: "", token: ""};
-					process.error(auth.message != null ? auth.message : "Something went wrong during authentication!");
+					promise.error(connect.message);
+					return;
 				}
-			} else
-				process.error("You're already logged in!");
-		} else
-			process.error("User data is missing (username and/or user token)!");
+			} else {
+				promise.error(auth.message);
+				return;
+			}
+		});
 
-		return process.future;
+		return promise.future;
 	}
 
 	/**
 	 * Close the current session and makes `null` the `session` variable.
-	 * @param onComplete Optional action when the request is done.
 	 */
-	public function logout(?onComplete:() -> Void) {
-		if (session != null) {
-			request('sessions', 'close');
-			if (!isSessionActive()) {
-				user = {name: "", token: ""};
-				session = null;
-			}
-		}
-		if (onComplete != null)
-			onComplete();
+	public function logout() {
+		request("sessions", "close");
+		user = {name: "", token: ""};
+		session = null;
 	}
 
 	/**
 	 * Makes the user to achieve a trophy.
+	 * Calls `getTrophiesList()` afterwards to update `session` variable data.
 	 * @param id The ID of the trophy to achieve.
 	 * @return A `Future` instance where you can set actions if this got success or failure.
 	 */
 	public function trophieAdd(id:Int):Future<Trophy> {
 		var promise = new Promise<Trophy>();
-		var process = request('trophies', 'add-achieved', ['trophy_id' => Std.string(id)]);
-		if (process.success == 'true' && session != null) {
-			var newTrophies = getTrophiesList();
-			if (newTrophies != null)
-				session.trophies = newTrophies;
 
-			for (t in session.trophies)
-				if (t.id == id) {
-					promise.complete(t);
-					break;
-				}
-		} else
-			promise.error(process.message != null ? process.message : "Something went wrong at trophie addition!");
+		Thread.create(function() {
+			var data = request('trophies', 'add-achieved', ['trophy_id' => Std.string(id)]);
+			promise.progress(1, 3);
+			if (data.message == null) {
+				var list = getTrophiesList();
+				promise.progress(2, 3);
+				if (list != null)
+					for (trophy in list)
+						if (trophy.id == id) {
+							promise.progress(3, 3);
+							promise.complete(trophy);
+							break;
+						}
+			} else
+				promise.error(data.message);
+		});
 
 		return promise.future;
 	}
 
 	/**
-	 * Removes an achieved trophy from the user, useful when you're about to test your game trophies.
+	 * Removes an achieved trophy from the user, useful when you're about to test your game trophies. \
+	 * Calls `getTrophiesList()` afterwards to update `session` variable data.
 	 * @param id The ID of the trophy to remove.
 	 * @return A `Future` instance where you can set actions if this got success or failure.
 	 */
 	public function trophieRemove(id:Int):Future<Trophy> {
 		var promise = new Promise<Trophy>();
-		var process = request('trophies', 'remove-achieved', ['trophy_id' => Std.string(id)]);
-		if (process.success == 'true' && session != null) {
-			var newTrophies = getTrophiesList();
-			if (newTrophies != null)
-				session.trophies = newTrophies;
 
-			for (t in session.trophies)
-				if (t.id == id) {
-					promise.complete(t);
-					break;
-				}
-		} else
-			promise.error(process.message != null ? process.message : "Something went wrong at trophie addition!");
+		Thread.create(function() {
+			var data = request('trophies', 'remove-achieved', ['trophy_id' => Std.string(id)]);
+			promise.progress(1, 3);
+			if (data.message == null) {
+				var list = getTrophiesList();
+				promise.progress(2, 3);
+				if (list != null)
+					for (trophy in list)
+						if (trophy.id == id) {
+							promise.progress(3, 3);
+							promise.complete(trophy);
+							break;
+						}
+			} else
+				promise.error(data.message);
+		});
 
 		return promise.future;
 	}
 
-	function getUserData(?id:OneOfTwo<Int, String>):Null<User> {
-		var userLoad = request('users', null, id != null ? [(id is Int ? 'user_id' : 'username') => Std.string(id)] : null, id == null, false);
-		if (userLoad.success == 'true') {
-			var daUser = userLoad.users[0];
-			var newPFP = daUser.avatar_url.substring(0, 32);
+	/**
+	 * Fetches information about one of more users, you can set them by their username or ID. \
+	 * It also updates the `info` field of the `session` variable if this succeed (this just in case you leave `ids` parameter null or empty).
+	 * @param ids The list of usernames and/or IDs of the user to fetch information from.
+	 * 				If you left this `null` or empty, this will return information about the user that's currently logged in.
+	 * 				NOTE: If you pass an ID, there must be ONLY IDs in the list, and if you pass a username,
+	 * 				there must be ONLY usernames in the list. Otherwise, this wouldn't work correctly.
+	 * @return The list of the users and their information if succeded.
+	 */
+	public function getUserData(?ids:Array<OneOfTwo<Int, String>>):Null<Array<User>> {
+		var idList:Array<String> = [];
+		if (ids != null)
+			for (id in ids)
+				idList.push(Std.string(id));
 
-			newPFP += '1000';
-			newPFP += daUser.avatar_url.substr(34);
-			daUser.avatar_url = newPFP;
-
-			return daUser;
-		}
-
-		return null;
-	}
-
-	function getFriendsList():Null<Array<User>> {
-		var data = request('friends');
-		if (data.success == 'true') {
-			var friends:Array<User> = [];
-			for (person in data.friends) {
-				var fetchPerson = getUserData(person.friend_id);
-				if (fetchPerson == null)
-					continue;
-
-				friends.push(fetchPerson);
+		var nullIDs:Bool = ids == null || ids == [];
+		var data = request('users', null, !nullIDs ? [(ids[0] is Int ? 'user_id' : 'username') => idList.join(",")] : null, nullIDs, false);
+		if (data.message == null) {
+			for (daUser in data.users) {
+				var newPFP = daUser.avatar_url.substring(0, 32);
+				newPFP += '1000';
+				newPFP += daUser.avatar_url.substr(34);
+				daUser.avatar_url = newPFP;
 			}
-			return friends;
+
+			if (session != null && nullIDs)
+				session.info = data.users[0];
+			return data.users;
 		}
 
+		trace(data.message);
 		return null;
 	}
 
-	function getTrophiesList():Null<Array<Trophy>> {
-		var trophList:Null<Array<Trophy>> = null;
+	/**
+	 * Fetches and updates the friends list of the user that's currently logged in. \
+	 * It also updates the `friends` field of the `session` variable if this succeed.
+	 * @return The list of the user's friends and their information if succeded.
+	 */
+	public function getFriendsList():Null<Array<User>> {
+		var data = request('friends');
+
+		if (data.message == null) {
+			var ids:Array<Int> = [];
+			for (daFriend in data.friends)
+				ids.push(daFriend.friend_id);
+
+			var list = getUserData(ids);
+			if (session != null && list != null)
+				session.friends = list;
+			return list;
+		}
+
+		trace(data.message);
+		return null;
+	}
+
+	/**
+	 * Fetches and updates the states of the trophies list from your game of the user that's currently logged in. \
+	 * It also updates the `trophies` field of the `session` variable if this succeed.
+	 * @return The list of the updated trophies states and their information if succeded.
+	 */
+	public function getTrophiesList():Null<Array<Trophy>> {
 		var data = request('trophies');
 
-		if (data.success == 'true') {
-			trophList = data.trophies;
-
-			for (t in trophList) {
+		if (data.message == null) {
+			for (t in data.trophies) {
 				var newUrl:String = "";
 				if (t.image_url.startsWith('https://m.')) {
 					newUrl = t.image_url.substring(0, 37);
@@ -304,42 +327,54 @@ class GJClient {
 				}
 				t.image_url = newUrl;
 			}
-		};
-		return trophList;
+			if (session != null)
+				session.trophies = data.trophies;
+			return data.trophies;
+		}
+
+		trace(data.message);
+		return null;
 	}
 
-	function isSessionActive():Bool
-		return request('sessions', 'check').success == 'true';
+	function pingSession() {
+		if (session == null)
+			return;
 
-	function request(command:String, ?action:String, ?params:Map<String, String>, userAllowed:Bool = true, tokenAllowed:Bool = true):Future<Response> {
-		Thread.create(function() {
-			var mainURL:String = "http://api.gamejolt.com/api/game/v1_2/";
-			var process:String = '$command${action != null ? '/$action' : ""}';
+		var ping = request("sessions", "ping");
+		if (ping.message != null) {
+			logout();
+			trace(ping.message);
+		}
+	}
 
-			mainURL += process;
-			mainURL += '/?game_id=${game.id}';
+	function request(command:String, ?action:String, ?params:Map<String, String>, userAllowed:Bool = true, tokenAllowed:Bool = true):Response {
+		var promise = new Promise<Response>();
+		var mainURL:String = "http://api.gamejolt.com/api/game/v1_2/";
+		var process:String = '$command${action != null ? '/$action' : ""}';
 
-			if (userAllowed)
-				mainURL += '&username=${user.name}';
-			if (tokenAllowed)
-				mainURL += '&user_token=${user.token}';
-			if (params != null)
-				for (k => v in params)
-					mainURL += '&$k=$v';
+		mainURL += process;
+		mainURL += '/?game_id=${game.id}';
 
-			var daEncode:String = mainURL + game.key;
-			mainURL += '&signature=${useMd5 ? Md5.encode(daEncode) : Sha1.encode(daEncode)}';
+		if (userAllowed)
+			mainURL += '&username=${user.name}';
+		if (tokenAllowed)
+			mainURL += '&user_token=${user.token}';
+		if (params != null)
+			for (k => v in params)
+				mainURL += '&$k=$v';
 
-			var response:Response;
-			var data = new Http(mainURL);
-			data.onData = req -> response = cast Json.parse(req).response;
-			data.onError = error -> response = {success: 'false', message: error};
-			data.request(false);
+		var daEncode:String = mainURL + game.key;
+		mainURL += '&signature=${useMd5 ? Md5.encode(daEncode) : Sha1.encode(daEncode)}';
 
-			if (response.message != null && debugNotes)
-				trace('"$process" => ${response.message}!');
-		});
-
+		var response:Response;
+		var data = new Http(mainURL);
+		data.onData = function(req) {
+			response = cast Json.parse(req.toString()).response;
+			if (response.message != null)
+				response.message = '"$process" => ${response.message}';
+		};
+		data.onError = e -> response = {success: "false", message: '"$process" => $e'};
+		data.request(false);
 		return response;
 	}
 }
