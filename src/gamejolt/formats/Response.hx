b1a888e521b5bb76920b7fef631bb6c121b67b66
GamerPablito <leonpablo1998@gmail.com>
2024-06-20 05:20:32 -0600

    Huge modifications!

--- src/gamejolt/formats/Response.hx
+++ src/gamejolt/formats/Response.hx
@@ -3,33 +3,32 @@ package gamejolt.formats;
 /**
  * This is how GameJolt API responses are formatted like.
  */
-@:structInit
-class Response {
+typedef Response = {
 	// General
-	public var success:Bool = false;
-	@:optional public var message:String = "No successful response has been received yet";
+	success:Bool,
+	?message:String,
 	// User Fetching
-	@:optional public var users:Array<User>;
+	?users:Array<User>,
 	// Trophies Fetching
-	@:optional public var trophies:Array<Trophy>;
+	?trophies:Array<Trophy>,
 	// Scores Fetching
-	@:optional public var scores:Array<Score>;
-	@:optional public var tables:Array<ScoreTable>;
-	@:optional public var rank:Int;
+	?scores:Array<Score>,
+	?tables:Array<ScoreTable>,
+	?rank:Int,
 	// Friends Fetching
-	@:optional public var friends:Array<{friend_id:Int}>;
+	?friends:Array<{friend_id:Int}>,
 	// Data Store Fetching
-	@:optional public var keys:Array<{key:String}>;
-	@:optional public var data:String;
+	?keys:Array<{key:String}>,
+	?data:String,
 	// Time Fetching
-	@:optional public var timestamp:Int;
-	@:optional public var timezone:String;
-	@:optional public var year:Int;
-	@:optional public var month:Int;
-	@:optional public var day:Int;
-	@:optional public var hour:Int;
-	@:optional public var minute:Int;
-	@:optional public var second:Int;
+	?timestamp:Int,
+	?timezone:String,
+	?year:Int,
+	?month:Int,
+	?day:Int,
+	?hour:Int,
+	?minute:Int,
+	?second:Int,
 	// Batch Reception
-	@:optional public var responses:Array<Response>;
+	?responses:Array<Response>
 }
